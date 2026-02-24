c67e0afc1f290328d7ea37adcd3f0eb8cbf0eba7
Yuvraj Bhalerao <152587402+YuvrajBhalerao@users.noreply.github.com>
2026-02-24 20:33:37 +0530

    Uncomment API router inclusion in main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import os
 
 app = FastAPI(title="CSA PDI Scenario Generator")
 
-# We will uncomment this once we build the api/routes.py file
-# from api.routes import router as api_router
-# app.include_router(api_router, prefix="/api")
+
+from api.routes import router as api_router
+app.include_router(api_router, prefix="/api")
 
 # Mount the 'static' folder to serve CSS, JS, and images
 # Make sure the 'static' folder exists in your root directory before running
